840cbfd1b5da350869580e402171721b5782faec
Sherida van den Bent <sherida@Sheridas-MacBook-Pro.local>
2020-06-20 15:01:08 +0200

    Chapter 5 our data - gravity and userAcceleration

--- code/Python3Code/crowdsignals_ch5.py
+++ code/Python3Code/crowdsignals_ch5.py
@@ -23,9 +23,9 @@ import matplotlib.pyplot as plt
 from pathlib import Path
 
 # As usual, we set our program constants, read the input file and initialize a visualization object.
-DATA_PATH = Path('./intermediate_datafiles/ours/')
+DATA_PATH = Path('./intermediate_datafiles/our_data/')
 DATASET_FNAME = sys.argv[1] if len(sys.argv) > 1 else 'chapter4_result.csv'
-RESULT_FNAME = sys.argv[2] if len(sys.argv) > 2 else 'chapter5_result_ours.csv'
+RESULT_FNAME = sys.argv[2] if len(sys.argv) > 2 else 'chapter5_result_gravity.csv'
 
 try:
     dataset = pd.read_csv(DATA_PATH / DATASET_FNAME, index_col=0)
@@ -48,7 +48,7 @@ silhouette_values = []
 print('===== kmeans clustering mag =====')
 for k in k_values:
     print(f'k = {k}')
-    dataset_cluster = clusteringNH.k_means_over_instances(copy.deepcopy(dataset), ['mag_phone_x', 'mag_phone_y', 'mag_phone_z'], k, 'default', 20, 10)
+    dataset_cluster = clusteringNH.k_means_over_instances(copy.deepcopy(dataset), ['gravity.x', 'gravity.y', 'gravity.z'], k, 'default', 20, 10)
     silhouette_score = dataset_cluster['silhouette'].mean()
     print(f'silhouette = {silhouette_score}')
     silhouette_values.append(silhouette_score)
@@ -62,10 +62,10 @@ DataViz.plot_xy(x=[k_values], y=[silhouette_values], xlabel='k', ylabel='silhoue
 k = k_values[np.argmax(silhouette_values)]
 print(f'Highest K-Means silhouette score: k = {k}')
 
-dataset_knn = clusteringNH.k_means_over_instances(copy.deepcopy(dataset), ['mag_phone_x', 'mag_phone_y', 'mag_phone_z'], k, 'default', 50, 50)
-DataViz.plot_clusters_3d(dataset_knn, ['mag_phone_x', 'mag_phone_y', 'mag_phone_z'], 'cluster', ['label'])
+dataset_knn = clusteringNH.k_means_over_instances(copy.deepcopy(dataset), ['gravity.x', 'gravity.y', 'gravity.z'], k, 'default', 50, 50)
+DataViz.plot_clusters_3d(dataset_knn, ['gravity.x', 'gravity.y', 'gravity.z'], 'cluster', ['label'])
 DataViz.plot_silhouette(dataset_knn, 'cluster', 'silhouette')
-util.print_latex_statistics_clusters(dataset_knn, 'cluster', ['mag_phone_x', 'mag_phone_y', 'mag_phone_z'], 'label')
+util.print_latex_statistics_clusters(dataset_knn, 'cluster', ['gravity.x', 'gravity.y', 'gravity.z'], 'label')
 del dataset_knn['silhouette']
 
 k_values = range(2, 10)
@@ -74,49 +74,49 @@ silhouette_values = []
 
 # Do some initial runs to determine the right number for k
 
-# print('===== k medoids clustering =====')
-# for k in k_values:
-#     print(f'k = {k}')
-#     dataset_cluster = clusteringNH.k_medoids_over_instances(copy.deepcopy(dataset), ['acc_phone_x', 'acc_phone_y', 'acc_phone_z'], k, 'default', 20, n_inits=10)
-#     silhouette_score = dataset_cluster['silhouette'].mean()
-#     print(f'silhouette = {silhouette_score}')
-#     silhouette_values.append(silhouette_score)
+print('===== k medoids clustering =====')
+for k in k_values:
+    print(f'k = {k}')
+    dataset_cluster = clusteringNH.k_medoids_over_instances(copy.deepcopy(dataset), ['gravity.x', 'gravity.y', 'gravity.z'], k, 'default', 20, n_inits=10)
+    silhouette_score = dataset_cluster['silhouette'].mean()
+    print(f'silhouette = {silhouette_score}')
+    silhouette_values.append(silhouette_score)
 
-# DataViz.plot_xy(x=[k_values], y=[silhouette_values], xlabel='k', ylabel='silhouette score k medoids',
-#                 ylim=[0,1], line_styles=['b-'])
+DataViz.plot_xy(x=[k_values], y=[silhouette_values], xlabel='k', ylabel='silhouette score k medoids',
+                ylim=[0,1], line_styles=['b-'])
 
-# # And run k medoids with the highest silhouette score
+# And run k medoids with the highest silhouette score
 
-# # k = 6 # todo: replaced with np.argmax call over silhouette scores
-# k = k_values[np.argmax(silhouette_values)]
-# print(f'Highest K-Medoids silhouette score: k = {k}')
+# k = 6 # todo: replaced with np.argmax call over silhouette scores
+k = k_values[np.argmax(silhouette_values)]
+print(f'Highest K-Medoids silhouette score: k = {k}')
 
-# dataset_kmed = clusteringNH.k_medoids_over_instances(copy.deepcopy(dataset), ['acc_phone_x', 'acc_phone_y', 'acc_phone_z'], k, 'default', 20, n_inits=50)
-# DataViz.plot_clusters_3d(dataset_kmed, ['acc_phone_x', 'acc_phone_y', 'acc_phone_z'], 'cluster', ['label'])
-# DataViz.plot_silhouette(dataset_kmed, 'cluster', 'silhouette')
-# util.print_latex_statistics_clusters(dataset_kmed, 'cluster', ['acc_phone_x', 'acc_phone_y', 'acc_phone_z'], 'label')
+dataset_kmed = clusteringNH.k_medoids_over_instances(copy.deepcopy(dataset), ['gravity.x', 'gravity.y', 'gravity.z'], k, 'default', 20, n_inits=50)
+DataViz.plot_clusters_3d(dataset_kmed, ['gravity.x', 'gravity.y', 'gravity.z'], 'cluster', ['label'])
+DataViz.plot_silhouette(dataset_kmed, 'cluster', 'silhouette')
+util.print_latex_statistics_clusters(dataset_kmed, 'cluster', ['gravity.x', 'gravity.y', 'gravity.z'], 'label')
 
-# # And the hierarchical clustering is the last one we try
+# And the hierarchical clustering is the last one we try
 
-# clusteringH = HierarchicalClustering()
+clusteringH = HierarchicalClustering()
 
-# k_values = range(2, 10)
-# silhouette_values = []
+k_values = range(2, 10)
+silhouette_values = []
 
-# # Do some initial runs to determine the right number for the maximum number of clusters.
+# Do some initial runs to determine the right number for the maximum number of clusters.
 
-# print('===== agglomerative clustering =====')
-# for k in k_values:
-#     print(f'k = {k}')
-#     dataset_cluster, l = clusteringH.agglomerative_over_instances(copy.deepcopy(dataset), ['acc_phone_x', 'acc_phone_y', 'acc_phone_z'], k, 'euclidean', use_prev_linkage=True, link_function='ward')
-#     silhouette_score = dataset_cluster['silhouette'].mean()
-#     print(f'silhouette = {silhouette_score}')
-#     silhouette_values.append(silhouette_score)
-#     if k == k_values[0]:
-#         DataViz.plot_dendrogram(dataset_cluster, l)
+print('===== agglomerative clustering =====')
+for k in k_values:
+    print(f'k = {k}')
+    dataset_cluster, l = clusteringH.agglomerative_over_instances(copy.deepcopy(dataset), ['gravity.x', 'gravity.y', 'gravity.z'], k, 'euclidean', use_prev_linkage=True, link_function='ward')
+    silhouette_score = dataset_cluster['silhouette'].mean()
+    print(f'silhouette = {silhouette_score}')
+    silhouette_values.append(silhouette_score)
+    if k == k_values[0]:
+        DataViz.plot_dendrogram(dataset_cluster, l)
 
-# DataViz.plot_xy(x=[k_values], y=[silhouette_values], xlabel='k', ylabel='silhouette score hierarchical',
-#                 ylim=[0,1], line_styles=['b-'])
+DataViz.plot_xy(x=[k_values], y=[silhouette_values], xlabel='k', ylabel='silhouette score hierarchical',
+                ylim=[0,1], line_styles=['b-'])
 
 # And we select the outcome dataset of the knn clustering....
 
